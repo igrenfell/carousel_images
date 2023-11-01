20dfe12316b4c3741dcffc09177db6a9806a6d29
igrenfell <igrenfell@gmail.com>
2023-11-01 16:17:58 -0600

    First commit

---
+++ image_proc.R
@@ -0,0 +1,36 @@
+library(EBImage)
+library(imager)
+
+
+rootwd <- getwd()
+setwd("image_files")
+
+folder_list <- list.files()
+
+nfolders <- length(folder_list)
+for(curfolder in 1:nfolders)
+{
+  setwd(rootwd)
+  setwd("image_files")
+  setwd(folder_list[curfolder])
+  
+  tiflist <- Sys.glob("*.tif")
+  ntifs <- length(tiflist)
+ for(curtif in 1:ntifs)
+ {
+   
+   curimg <- readImage(tiflist[curtif])
+   img.gray <-curimg[,,1:4]
+   
+   img.gray <- curimg
+   colorMode(img.gray) <- Grayscale
+   thresh.gray <-thresh(img.gray, 10, 10, 0.15)
+  # plot(thresh.gray)
+   kern <- makeBrush(5, shape='disc') 
+   thresh.array <- array(thresh.gray, dim = dim(thresh.gray) )
+   dilate.gray <- EBImage::dilate(thresh.array,kern)
+   
+   
+ }
+   
+}
